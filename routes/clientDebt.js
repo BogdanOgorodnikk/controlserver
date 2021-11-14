37c68d43a7a27717ea1381b32c695f086bfbd71d
bogdanOgorodnik <superpm7@gmail.com>
2021-11-14 14:47:27 +0200

    Fix

--- routes/clientDebt.js
+++ routes/clientDebt.js
@@ -5,6 +5,19 @@ const { sequelize } = require('../database/db')
 const authMiddleware = require('../middleware/auth.middleware')
 
 router.get('/api/alldebts', authMiddleware, async ctx => {
+    const start = ctx.query.start;
+    const end = ctx.query.end;
+
+    let string = '';
+
+    if(start && !end) {
+        string = `and DATE(orders.data) >= '${start}'`
+    } else if(end && !start) {
+        string = `and DATE(orders.data) <= '${end}'`
+    } else if(start && end) {
+        string = `and DATE(orders.data) BETWEEN '${start}' AND '${end}'`
+    }
+
     try {
         if(ctx.user.role_id != 1 && ctx.user.role_id !=5 && ctx.user.role_id !=4 || ctx.user.ban == 1) {
             return ctx.status = 400
@@ -15,7 +28,7 @@ router.get('/api/alldebts', authMiddleware, async ctx => {
                 FROM orders 
                 LEFT JOIN clients ON orders.client_id = clients.id
                 JOIN towns ON clients.town_id = towns.id
-                WHERE orders.product_name != "Перевірка"
+                WHERE orders.product_name != "Перевірка" ${string}
                 GROUP BY clients.id
                 `
             )

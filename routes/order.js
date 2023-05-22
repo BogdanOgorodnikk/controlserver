4bec3ce7acefad7d0f915a3c2316835f8c8e186d
bogdanOgorodnik <superpm7@gmail.com>
2023-05-22 22:44:04 +0300

    a

--- routes/order.js
+++ routes/order.js
@@ -8,14 +8,6 @@ const Comment = require("../models/Comment");
 
 router.get('/api/orders/:client_id', authMiddleware, async ctx => {
     const client_id = ctx.params.client_id
-    const start = ctx.query.start;
-    const end = ctx.query.end;
-
-    let [startDay, startMonth, startYear] = start.split(".");
-    let [day, month, year] = end.split(".");
-
-    const preparedDataStart = format(new Date(startYear, startMonth - 1, startDay), "yyyy-MM-dd");
-    const preparedDataEnd = format(new Date(year, month - 1, day), "yyyy-MM-dd");
 
     try {
         if(ctx.user.role_id < 1 || ctx.user.role_id > 5 || ctx.user.ban == 1) {
@@ -32,7 +24,7 @@ router.get('/api/orders/:client_id', authMiddleware, async ctx => {
                     orders.creater, orders.region, orders.debt, orders.client_id, users.login
                     FROM orders 
                     LEFT JOIN users ON orders.creater = users.id
-                    where client_id = ${client_id} AND DATE(orders.data) BETWEEN '${preparedDataStart}' AND '${preparedDataEnd}'
+                    where client_id = ${client_id}
                     ORDER BY orders.id`
             )
 
@@ -46,7 +38,7 @@ router.get('/api/orders/:client_id', authMiddleware, async ctx => {
                  prepare_orders.creater, prepare_orders.region, prepare_orders.client_id, users.login
                  FROM prepare_orders
                  LEFT JOIN users ON prepare_orders.creater = users.id
-                 WHERE prepare_orders.order_number = "" and prepare_orders.client_id = ${client_id} and DATE(prepare_orders.data) BETWEEN '${preparedDataStart}' AND '${preparedDataEnd}'
+                 WHERE prepare_orders.order_number = "" and prepare_orders.client_id = ${client_id}
                  ORDER BY prepare_orders.id`
             );
 
@@ -65,7 +57,7 @@ router.get('/api/orders/:client_id', authMiddleware, async ctx => {
                 `SELECT id, isSelfCar, order_number, orders.note, orders.comment, car_number, firm, DATE_FORMAT(data, '%d.%m.%Y') as data, 
                     product_name, opt_price, count, delivery_cash, delivery_cashless, region, original_data_create 
                 FROM orders 
-                where client_id = ${client_id} and firm != "" and orders.pay_cashless = 0 and DATE(orders.data) BETWEEN '${preparedDataStart}' AND '${preparedDataEnd}'
+                where client_id = ${client_id} and firm != "" and orders.pay_cashless = 0
                 ORDER BY id`
             )
 
@@ -77,7 +69,7 @@ router.get('/api/orders/:client_id', authMiddleware, async ctx => {
                  prepare_orders.count,
                  prepare_orders.region
                  FROM prepare_orders
-                 WHERE prepare_orders.order_number = "" and prepare_orders.client_id = ${client_id} and DATE(prepare_orders.data) BETWEEN '${preparedDataStart}' AND '${preparedDataEnd}'
+                 WHERE prepare_orders.order_number = "" and prepare_orders.client_id = ${client_id}
                  ORDER BY prepare_orders.id`
             );
 
@@ -95,7 +87,7 @@ router.get('/api/orders/:client_id', authMiddleware, async ctx => {
             const order = await sequelize.query(
                 `SELECT id, isSelfCar, order_number, orders.note, orders.comment, car_number, if(orders.pay_cashless = 0, orders.firm, "") as firm, DATE_FORMAT(data, '%d.%m.%Y') as data, product_name, opt_price, price_cash, price_cashless, count, sumseller, delivery_cash, delivery_cashless, pay_cash, pay_cashless, region
                 FROM orders 
-                where client_id = ${client_id} and DATE(orders.data) BETWEEN '${preparedDataStart}' AND '${preparedDataEnd}'
+                where client_id = ${client_id}
                 ORDER BY id`
             )
             return ctx.body = {
@@ -108,7 +100,7 @@ router.get('/api/orders/:client_id', authMiddleware, async ctx => {
                 product_name, opt_price, price_cash, delta_mas_cashless, price_cashless, count,
                 delivery_cash, delivery_cashless, pay_cashless, region, general_sum, pay_cash
                 FROM orders 
-                where client_id = ${client_id} and DATE(orders.data) BETWEEN '${preparedDataStart}' AND '${preparedDataEnd}'
+                where client_id = ${client_id}
                 ORDER BY id`
             )
             return ctx.body = {
@@ -135,7 +127,7 @@ router.get('/api/orders/:client_id', authMiddleware, async ctx => {
                     orders.pay_cash, orders.pay_cashless, orders.region, users.login
                 FROM orders 
                 LEFT JOIN users ON orders.creater = users.id
-                where client_id = ${client_id} and DATE(orders.data) BETWEEN '${preparedDataStart}' AND '${preparedDataEnd}'
+                where client_id = ${client_id}
                 ORDER BY id`
             )
 
@@ -148,7 +140,7 @@ router.get('/api/orders/:client_id', authMiddleware, async ctx => {
                  prepare_orders.count,
                  prepare_orders.region
                  FROM prepare_orders
-                 WHERE prepare_orders.order_number = "" and prepare_orders.client_id = ${client_id} and DATE(prepare_orders.data) BETWEEN '${preparedDataStart}' AND '${preparedDataEnd}'
+                 WHERE prepare_orders.order_number = "" and prepare_orders.client_id = ${client_id}
                  ORDER BY prepare_orders.id`
             );
 
@@ -595,7 +587,7 @@ router.put('/api/pricecash/:id', authMiddleware, async ctx => {
 })
 
 router.put('/api/pricecashless/:id', authMiddleware, async ctx => {
-    const {price_cashless} = ctx.request.body
+    const {price_cashless, price_cash} = ctx.request.body
     try {
         if(ctx.user.role_id !=4 || ctx.user.ban == 1) {
             return ctx.status = 400
@@ -604,11 +596,14 @@ router.put('/api/pricecashless/:id', authMiddleware, async ctx => {
             `SELECT * FROM orders where id = ${ctx.params.id}`
         )
 
+        const newPriceCash = price_cash ? price_cash : orderInfo[0][0].price_cash
+
         const order = await Order.update(
             {
                 original_data_update: orderInfo[0][0].original_data_update ? orderInfo[0][0].original_data_update : new Date(),
                 price_cashless: price_cashless,
-                delta_cash: ((orderInfo[0][0].price_cash-price_cashless)*orderInfo[0][0].count) - orderInfo[0][0].delivery_cash,
+                price_cash: newPriceCash,
+                delta_cash: ((newPriceCash-price_cashless)*orderInfo[0][0].count) - orderInfo[0][0].delivery_cash,
                 delta_cashless: ((price_cashless - orderInfo[0][0].opt_price) * orderInfo[0][0].count) - orderInfo[0][0].delivery_cashless,
             },
             {where: {id: ctx.params.id}}
@@ -686,7 +681,6 @@ router.put('/api/editorder/:id', authMiddleware, async ctx => {
         }
 
         if(ctx.user.role_id === 2) {
-            const price_cash = orderInfo[0][0].price_cash
             const price_cashless = orderInfo[0][0].price_cashless
 
             const order = await Order.update(
@@ -703,6 +697,7 @@ router.put('/api/editorder/:id', authMiddleware, async ctx => {
                     delivery_cash: delivery_cash,
                     delivery_cashless: delivery_cashless,
                     opt_price: opt_price,
+                    price_cash: price_cash,
                     general_sum: (price_cash * count) - delivery_cash,
                     debt: (price_cash * count) - delivery_cash,
                     sumseller: price_cash * count,
@@ -746,7 +741,7 @@ router.put('/api/editorder/:id', authMiddleware, async ctx => {
 
         if(ctx.user.role_id === 2) {
             const newOrder = await sequelize.query(
-                `SELECT orders.id, orders.order_number, orders.isSelfCar, orders.note, orders.comment, orders.car_number, orders.firm,
+                `SELECT orders.id, orders.order_number, orders.isSelfCar, orders.price_cash, orders.note, orders.comment, orders.car_number, orders.firm,
                 DATE_FORMAT(orders.data, '%d.%m.%Y') as data,
                 orders.product_name, orders.opt_price, orders.count, 
                 orders.delivery_cash, orders.delivery_cashless,
